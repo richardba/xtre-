182695901b3f8101140961fe22cec9f1c94646bf
Ricardo Oliveira <richardtrle@gmail.com>
2015-07-29 15:30:14 +0000

    fixes

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
-$(window).load(function() {
-	$(document).on("click", ".star", function(event) {
-		var $this = $(this);
-		var i = $this.attr('value');
-		$('#rating').val(i)
-		for (i; i & gt; 0; i--) {
-			$(".star[value=" + i + "]").toggleClass('star_active star');
+$(document).on("click", ".star", function (event) {
+    var $this = $(this);
+    var i=$this.attr('value');
+    $('#rating').val(i)
+    for(i;i>0;i--)
+    {
+        $(".star[value="+i+"]").toggleClass('star_active star');
 
 
-		}
-		event.preventDefault();
+    }
+    event.preventDefault();
 
-	});
-	$(document).on("click", ".star_active", function(event) {
-		var $this = $(this);
-		var i = parseInt($this.attr('value')) + 1;
-		$('#rating').val(i - 1)
-		for (i; i & lt; = 5; i++) {
-			$(".star_active[value=" + i + "]").toggleClass('star star_active');
-		}
-		event.preventDefault();
-	});
+});
+$(document).on("click", ".star_active", function (event) {
+    var $this = $(this);
+    var i=parseInt($this.attr('value'))+1;
+    $('#rating').val(i-1)
+    for(i;i<=5;i++)
+    {
+        $(".star_active[value="+i+"]").toggleClass('star star_active');
+    }
+    event.preventDefault();
 });
